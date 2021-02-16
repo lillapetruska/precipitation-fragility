ed31b55e49483258e3c222bcdf14621eda50c06c
Ryan <ryan@Ryans-Mac.local>
2021-02-16 14:58:52 -0800

    Update 2-16

--- scripts/assignment2.R
+++ scripts/assignment2.R
@@ -9,6 +9,7 @@ library(lubridate)
 library(here)
 library(raster)
 library(fixest)
+library(exactextractr)
 library(sf)
 # Parameters
 fsi <- read_rds(here::here("data/fsi.rds"))
@@ -74,17 +75,17 @@ v <-
 
 #-------------------------------------------------------------------------------
 precip_raster <- raster::brick(precip_filepath)
-chirps_raster <- raster::brick(chirps_filepath)
+#chirps_raster <- raster::brick(chirps_filepath)
 
-names(chirps_raster)
+#names(chirps_raster)
  # subset based on names
 precip_2006_2020 <- precip_raster %>% subset(325:504)
 
-chirps_raster <- subset(chirps_raster, c("X2006.01.01", "X2007.01.01", "X2008.01.01",
-                                         "X2009.01.01", "X2010.01.01", "X2011.01.01", "X2012.01.01", "X2013.01.01", "X2014.01.01",
-                                         "X2015.01.01", "X2016.01.01", "X2017.01.01", "X2018.01.01", "X2019.01.01", "X2020.01.01"))
+#chirps_raster <- subset(chirps_raster, c("X2006.01.01", "X2007.01.01", "X2008.01.01",
+                                       #  "X2009.01.01", "X2010.01.01", "X2011.01.01", "X2012.01.01", "X2013.01.01", "X2014.01.01",
+                                        # "X2015.01.01", "X2016.01.01", "X2017.01.01", "X2018.01.01", "X2019.01.01", "X2020.01.01"))
 
-prec <- raster::calc(chirps_raster, fun = mean, na.rm=T)
+#prec <- raster::calc(chirps_raster, fun = mean, na.rm=T)
 
 # Read in country boundaries shapefile
 countries <-
@@ -95,31 +96,31 @@ countries <-
 
 country_index <- countries$SOVEREIGNT %>% as_tibble() %>% mutate(ID = row_number())
 
-precip_by_country <- raster::extract(precip_2006_2020, countries, small = T, df = TRUE)
+#precip_by_country <- raster::extract(precip_2006_2020, countries, small = T, df = TRUE)
 
-annual_precip <-
-  precip_by_country %>%
-  pivot_longer(cols = starts_with("X"), names_to = "month", values_to = "precip") %>%
-  group_by(ID, month) %>%
-  summarize(avg_monthly_prec = mean(precip, na.rm = TRUE)) %>%
-  ungroup() %>%
-  mutate(month = str_extract(month, "\\d{4}")) %>%
-  group_by(ID, month) %>%
-  summarize(annual_precip = sum(avg_monthly_prec, na.rm = TRUE)) %>%
-  ungroup() %>%
-  left_join(country_index, by = "ID")
+#annual_precip <-
+  # precip_by_country %>%
+  # pivot_longer(cols = starts_with("X"), names_to = "month", values_to = "precip") %>%
+  # group_by(ID, month) %>%
+  # summarize(avg_monthly_prec = mean(precip, na.rm = TRUE)) %>%
+  # ungroup() %>%
+  # mutate(month = str_extract(month, "\\d{4}")) %>%
+  # group_by(ID, month) %>%
+  # summarize(annual_precip = sum(avg_monthly_prec, na.rm = TRUE)) %>%
+  # ungroup() %>%
+  # left_join(country_index, by = "ID")
 
 #Test1
 
 precip_by_country <- exact_extract(precip_2006_2020, countries)
 
-lapply(precip_by_country[[1]], sum) %>% unlist()
+#lapply(precip_by_country[[1]], sum) %>% unlist()
 
 #precip_by_country %>% map(~ .[[.]] %>% sum(.) %>% unlist(.))
 
 y <- data.frame()
 for (i in 1:178) {
-  v <- lapply(precip_by_country[[i]], sum) %>% unlist()
+  v <- lapply(precip_by_country[[i]], mean) %>% unlist()
   y <- bind_rows(y, v)
 }
 
@@ -127,16 +128,22 @@ y <- y %>% mutate(ID=row_number()) %>% left_join(country_index, by = "ID")
 
 y %>% pivot_longer(cols = starts_with("X"), names_to = "month", values_to = "precip")
 
-y %>% 
-  select(-coverage_fraction, -ID) %>%
+annual <- y %>% 
+  dplyr::select(-coverage_fraction, -ID) %>%
   pivot_longer(cols = starts_with("X"), names_to = "month", values_to = "precip") %>%
-  group_by_value
-  
+  mutate(month=str_remove(month, "X"), month=str_replace_all(month, "\\.", "-")) %>%
+  mutate(month=ymd(month)) %>%
+  mutate(precip=case_when(
+    month(month) %in% c(1, 3, 5, 7, 8, 10, 12) ~ precip * 31,
+    month(month) %in% c(4, 6, 9, 11) ~ precip * 30,
+    month(month) %in% c(2) ~ precip * 28.25
+  )) %>%
+  mutate(year=year(month)) %>% 
+  dplyr::select(-month) %>% 
+  group_by(value, year) %>% 
+  summarize(yearly_prec = sum(precip, na.rm = TRUE))
 
-  group_by(ID, month) %>%
-  summarize(avg_monthly_prec = mean(precip, na.rm = TRUE)) %>%
-  ungroup() %>%
-  mutate(month = str_extract(month, "\\d{4}")) %>%
-  group_by(ID, month) %>%
-  summarize(annual_precip = sum(avg_monthly_prec, na.rm = TRUE)) %>%
-  ungroup() %>%
+countries %>% left_join(annual, by=c("SOVEREIGNT"="value")) %>% filter(year==2020) %>% 
+write_rds("~/")
+
+plot(precip_2006_2020$X2007.09.01)
